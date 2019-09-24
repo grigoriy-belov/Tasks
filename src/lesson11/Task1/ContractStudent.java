eaf10d52d77603a7c1b6b27edd0bb8e5b05f1560
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-09-24 14:26:44 +0300

    Create ContractStudent class

---
+++ src/lesson11/Task1/ContractStudent.java
@@ -0,0 +1,27 @@
+package lesson11.Task1;
+
+public class ContractStudent extends Student {
+    private double contractPrice;
+
+    public ContractStudent(String name, int age, double contractPrice) {
+        super(name, age);
+        this.contractPrice = contractPrice;
+    }
+
+    public double getContractPrice() {
+        return contractPrice;
+    }
+
+    public void setContractPrice(double contractPrice) {
+        this.contractPrice = contractPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "ContractStudent{" +
+                "contractPrice=" + contractPrice +
+                ", name='" + super.getName() + '\'' +
+                ", age=" + super.getAge() +
+                '}';
+    }
+}
