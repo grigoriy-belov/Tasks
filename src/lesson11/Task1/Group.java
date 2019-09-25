5c413c7026d7db53e5788a16e0de65f0aad6cc16
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-09-25 12:21:31 +0300

    Extract main method to the StudntGroupApp class

--- src/lesson11/Task1/Group.java
+++ src/lesson11/Task1/Group.java
@@ -9,18 +9,7 @@ public class Group {
         count = 0;
     }
 
-    public static void main(String[] args) {
-        Group group = new Group(5);
-        group.addStudent(new Student("Ivanov", 22));
-        group.addStudent(new ContractStudent("Petrov", 25, 800));
-        group.addStudent(new Student("Sidorov", 27));
-        group.addStudent(new ContractStudent("Volkova", 23, 700));
-        group.addStudent(new Student("Zayceva", 23));
-
-        group.printContractStudentsInfo(group);
-    }
-
-    private void printContractStudentsInfo(Group group) {
+    public void printContractStudentsInfo(Group group) {
         for (int i = 0; i < group.students.length; i++) {
             if (group.students[i] instanceof ContractStudent) {
                 ContractStudent student = (ContractStudent) students[i];
@@ -29,7 +18,7 @@ public class Group {
         }
     }
 
-    private void addStudent(Student student) {
+    public void addStudent(Student student) {
         if (count == students.length) {
             System.out.println("The group is full");
         } else {
---
+++ src/lesson11/Task1/StudentGroupApp.java
@@ -0,0 +1,15 @@
+package lesson11.Task1;
+
+public class StudentGroupApp {
+
+    public static void main(String[] args) {
+        Group group = new Group(5);
+        group.addStudent(new Student("Ivanov", 22));
+        group.addStudent(new ContractStudent("Petrov", 25, 800));
+        group.addStudent(new Student("Sidorov", 27));
+        group.addStudent(new ContractStudent("Volkova", 23, 700));
+        group.addStudent(new Student("Zayceva", 23));
+
+        group.printContractStudentsInfo(group);
+    }
+}
