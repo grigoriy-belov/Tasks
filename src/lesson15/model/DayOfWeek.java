1dceb731c858bc354a35a09e9bdfdce7925c79c3
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-10-07 19:24:03 +0300

    Add model package

--- src/lesson15/model/DayOfWeek.java
+++ src/lesson15/model/DayOfWeek.java
@@ -1,4 +1,4 @@
-package lesson15;
+package lesson15.model;
 
 public enum DayOfWeek {
     MON("Monday"),
--- src/lesson15/model/Month.java
+++ src/lesson15/model/Month.java
@@ -1,4 +1,4 @@
-package lesson15;
+package lesson15.model;
 
 public enum Month {
     JAN("January", 31),
