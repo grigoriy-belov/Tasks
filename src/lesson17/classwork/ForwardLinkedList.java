61a65cf0c6c990e476e6b4bfc7bce0a28e98fe84
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-10-21 20:12:24 +0300

    ForwardLinkedList partial realization2

--- src/lesson17/classwork/ForwardLinkedList.java
+++ src/lesson17/classwork/ForwardLinkedList.java
@@ -1,5 +1,8 @@
 package lesson17.classwork;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ForwardLinkedList<T> {
 
     private Node<T> head;
@@ -12,6 +15,23 @@ public class ForwardLinkedList<T> {
         } else {
             tail().link(new Node<>(element));
         }
+        size++;
+    }
+
+    public void add(int index, T element) {
+        Node<T> previous = navigate(index -1);
+        Node<T> next = previous.next();
+        Node<T> added = new Node<T>(element, next);
+        previous.link(added);
+        size++;
+    }
+
+    public void remove(int index) {
+        Node<T> removed = navigate(index);
+    }
+
+    public T get(int index) {
+        return navigate(index).getValue();
     }
 
     public boolean isEmpty() {
@@ -25,6 +45,15 @@ public class ForwardLinkedList<T> {
 
     public int size() {
         return size;
+    }
+
+    private Node<T> navigate(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> n = head;
+        for (int i = 0; i < index; i++) {
+            n = head.next();
+        }
+        return n;
     }
 
     private Node<T> tail() {
