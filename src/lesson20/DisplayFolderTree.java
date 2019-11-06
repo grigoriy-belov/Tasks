189fcbe9c11ddfb588de299494d6c740cf69cd50
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-11-06 10:14:32 +0200

    Bug fix

--- src/lesson20/DisplayFolderTree.java
+++ src/lesson20/DisplayFolderTree.java
@@ -1,60 +1,54 @@
 package lesson20;
 
 import java.io.File;
+import java.util.Objects;
 
 public class DisplayFolderTree {
 
-
     public static void main(String[] args) {
-        DisplayFolderTree displayFolderTree = new DisplayFolderTree();
         File folder = new File(args[0]);
-        System.out.println(displayFolderTree.printDirectoryTree(folder));
+        System.out.println(printFolderTree(folder));
     }
 
-    public static String printDirectoryTree(File folder) {
+    public static String printFolderTree(File folder) {
         if (!folder.isDirectory()) {
             throw new IllegalArgumentException("folder is not a Directory");
         }
         int indent = 0;
         StringBuilder sb = new StringBuilder();
-        printDirectoryTree(folder, indent, sb);
+        printFolderTree(folder, indent, sb);
         return sb.toString();
     }
 
-    private static void printDirectoryTree(File folder, int indent, StringBuilder sb) {
-
+    private static void printFolderTree(File folder, int indent, StringBuilder sb) {
+        if (!folder.isDirectory()) {
+            throw new IllegalArgumentException("argument is not directory");
+        }
         if (indent == 0) {
             sb.append(folder.getName());
             sb.append("\n");
         } else {
-            sb.append(getIndentString(indent));
-            sb.append("+---");
-            sb.append(folder.getName());
-            sb.append("\n");
+            printFile(folder, indent, sb);
         }
 
-        for (File file : folder.listFiles()) {
-            if (file.isDirectory()) {
-                printDirectoryTree(file, indent + 1, sb);
-            } else {
+        for (File file : Objects.requireNonNull(folder.listFiles())) {
+            if (file.isFile()) {
                 printFile(file, indent + 1, sb);
+            } else if (file.isDirectory()) {
+                printFolderTree(file, indent + 1, sb);
             }
         }
     }
 
     private static void printFile(File file, int indent, StringBuilder sb) {
-        sb.append(getIndentString(indent));
+        printIndentString(indent, sb);
         sb.append("+---");
         sb.append(file.getName());
         sb.append("\n");
     }
 
-    private static String getIndentString(int indent) {
-        StringBuilder sb = new StringBuilder();
-        if (indent > 1) {
-            sb.append("+\t".repeat(indent));
-        }
-        return sb.toString();
+    private static void printIndentString(int indent, StringBuilder sb) {
+        sb.append("+\t".repeat(Math.max(0, indent - 1)));
     }
 }
 
