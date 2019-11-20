df8efd4801471422ac95dafe841d5033b6c4b028
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-11-20 13:45:03 +0200

    Initial commit

--- textboard-jdbc-censor/src/main/java/com/alevel/textboard/Censorship.java
+++ textboard-jdbc-censor/src/main/java/com/alevel/textboard/Censorship.java
@@ -1,23 +1,58 @@
 package com.alevel.textboard;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.List;
 
 public class Censorship {
     public static void main(String[] args) {
         try (Connection connection = ConnectionFactory.connect()) {
-            try (Statement findAllComments = connection.createStatement()) {
-                ResultSet comments = findAllComments.executeQuery("SELECT text FROM comments;");
+            connection.setAutoCommit(false);
+            String updateComments = "UPDATE comments SET text = ? WHERE id = ?;";
+            String updateThreads = "UPDATE threads SET title = ?, text = ? WHERE id = ?;";
+
+            try (Statement statement = connection.createStatement();
+                 PreparedStatement psComments = connection.prepareStatement(updateComments);
+                 PreparedStatement psThreads = connection.prepareStatement(updateThreads)) {
+
+                FileLoader fileLoader = new FileLoader("textboard-jdbc-censor\\blacklist.csv");
+                List<String> blackList = fileLoader.getBlackList();
+                ResultSet commentsSet = statement.executeQuery("SELECT * FROM comments;");
+
+                while (commentsSet.next()) {
+                    String comment = commentsSet.getString("text");
+                    Long id = commentsSet.getLong("id");
+
+                    for (String word : blackList) {
+                        comment = comment.replaceAll("(?i)" + word, "*");
+                    }
+                    psComments.setString(1, comment);
+                    psComments.setLong(2, id);
+                    psComments.executeUpdate();
+                }
+
+                ResultSet threadsSet = statement.executeQuery("SELECT id, title, text FROM threads;");
+
+                while (threadsSet.next()) {
+                    String text = threadsSet.getString("text");
+                    String title = threadsSet.getString("title");
+                    Long id = threadsSet.getLong("id");
+
+                    for (String word : blackList) {
+                        text = text.replaceAll("(?i)" + word, "*");
+                        title = title.replaceAll("(?i)" + word, "*");
+                    }
+                    psThreads.setString(1, title);
+                    psThreads.setString(2, text);
+                    psThreads.setLong(3, id);
+                    psThreads.executeUpdate();
+                }
+                connection.commit();
             }
 
-
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } ;
+        }
     }
+
 }
--- textboard-jdbc-censor/src/main/java/com/alevel/textboard/FileLoader.java
+++ textboard-jdbc-censor/src/main/java/com/alevel/textboard/FileLoader.java
@@ -1,9 +1,38 @@
 package com.alevel.textboard;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class FileLoader {
-    public List<String> getBlackList() {
 
+    private String filePath;
+    private Path path;
+
+
+    public FileLoader(String filePath) {
+        this.filePath = filePath;
+        path = Paths.get(filePath);
+    }
+
+    public List<String> getBlackList() {
+        List<String> lines = null;
+        List<String> blackList = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String line : lines) {
+            blackList.addAll(Arrays.asList(line.split(", ")));
+        }
+        return blackList;
     }
 }
