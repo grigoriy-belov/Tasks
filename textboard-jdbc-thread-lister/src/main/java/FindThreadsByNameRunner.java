2ebba04b1fcf78aba82f0c5c6efe0e96f2a43e80
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-11-22 11:57:27 +0200

    Display multilevel replies realization

--- textboard-jdbc-thread-lister/src/main/java/FindThreadsByNameRunner.java
+++ textboard-jdbc-thread-lister/src/main/java/FindThreadsByNameRunner.java
@@ -15,36 +15,49 @@ import java.util.List;
 public class FindThreadsByNameRunner {
     public static void main(String[] args) throws IOException, SQLException {
         try (Connection c = ConnectionFactory.connect()) {
-            ThreadDAO threadDAO = new JDBCThreadDAO(c);
-            CommentDAO commentDAO = new JDBCCommentDAO(c);
-            List<Persisted<Long, Thread>> threads = threadDAO.findByName("world");
-            StringBuilder sb = new StringBuilder();
-
-            for (Persisted<Long, Thread> thread : threads) {
-
-                User user = thread.getValue().getUser();
-                sb.append(thread.getValue().getTitle().toUpperCase())
-                        .append(" (").append(user.getNickname()).append(")")
-                        .append("\n").append(thread.getValue().getText())
-                        .append("\n");
-
-                List<Persisted<Long, Comment>> comments = commentDAO.findByThreadId(thread.getId());
-
-                for (Persisted<Long, Comment> comment : comments) {
-                    User author = comment.getValue().getAuthor().getValue();
-                    String text = comment.getValue().getText();
-                    if (comment.getValue().getParentComment() == null) {
-                        sb.append(author.getNickname()).append(": ")
-                                .append(text).append("\n");
-                        for (Comment reply : comment.getValue().getReplies()) {
-                            sb.append("--- ").append(reply.getAuthor())
-                                    .append(": ").append(reply.getText());
-                        }
-                    }
-                }
-                sb.append("\n");
-            }
-            System.out.println(sb.toString());
+            System.out.println(getThreadsOutput("hello", new JDBCThreadDAO(c), new JDBCCommentDAO(c)));
         }
+    }
+
+    private static String getThreadsOutput(String threadName, ThreadDAO threadDAO, CommentDAO commentDAO) {
+        List<Persisted<Long, Thread>> threads = threadDAO.findByName(threadName);
+        StringBuilder sb = new StringBuilder();
+
+        for (Persisted<Long, Thread> thread : threads) {
+
+            User user = thread.getValue().getUser();
+            sb.append(thread.getValue().getTitle().toUpperCase())
+                    .append(" (").append(user.getNickname()).append(")")
+                    .append("\n").append(thread.getValue().getText())
+                    .append("\n");
+
+            List<Persisted<Long, Comment>> comments = commentDAO.findByThreadId(thread.getId());
+
+            for (Persisted<Long, Comment> comment : comments) {
+                User author = comment.getValue().getAuthor().getValue();
+                String text = comment.getValue().getText();
+                if (comment.getValue().getParentComment() == null) {
+                    sb.append(author.getNickname()).append(": ")
+                            .append(text).append("\n");
+                    getRepliesOutput(sb, comment.getValue(), 1);
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static String getRepliesOutput(StringBuilder sb, Comment comment, int indent) {
+        for (Comment reply : comment.getReplies()) {
+            for (int i = 0; i < indent; i++) {
+                sb.append("--- ");
+            }
+            sb.append(reply.getAuthor().getValue().getNickname())
+                    .append(": ").append(reply.getText()).append("\n");
+            if (reply.getReplies() != null) {
+                getRepliesOutput(sb, reply, indent + 1);
+            }
+        }
+        return sb.toString();
     }
 }
--- textboard-jdbc-thread-lister/src/main/java/dao/comment/JDBCCommentDAO.java
+++ textboard-jdbc-thread-lister/src/main/java/dao/comment/JDBCCommentDAO.java
@@ -1,8 +1,6 @@
 package dao.comment;
 
 import dao.DataAccessException;
-import dao.user.JDBCUserDAO;
-import dao.user.UserDAO;
 import model.Comment;
 import model.Persisted;
 import model.Thread;
@@ -14,7 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class JDBCCommentDAO implements CommentDAO {
     private final Connection connection;
@@ -25,11 +22,27 @@ public class JDBCCommentDAO implements CommentDAO {
 
     @Override
     public List<Persisted<Long, Comment>> findByThreadId(Long threadId) {
-        String sql = "SELECT c.id, parent_comment_id, email, nickname, title, c.text, author_id, thread_id " +
+        String sql = "SELECT c.id, parent_comment_id, email, nickname, title, c.text, t.text, author_id, thread_id " +
                     "FROM comments AS c " +
                     "JOIN users AS u ON c.author_id = u.id " +
                     "JOIN threads AS t ON c.thread_id = t.id " +
-                    "WHERE thread_id = ?;";
+                    "WHERE thread_id = ? AND parent_comment_id IS NULL " +
+                     "ORDER BY c.id ;";
+
+        return findOne(threadId, sql);
+    }
+
+    public List<Persisted<Long, Comment>> findByParentId(long id) {
+        String sql = "SELECT c.id, parent_comment_id, email, nickname, title, c.text, t.text, author_id, thread_id " +
+                "FROM comments AS c " +
+                "JOIN users AS u ON c.author_id = u.id " +
+                "JOIN threads AS t ON c.thread_id = t.id " +
+                "WHERE parent_comment_id = ? " +
+                "ORDER BY c.id;";
+        return findOne(id, sql);
+    }
+
+    private List<Persisted<Long, Comment>> findOne(Long threadId, String sql) {
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setLong(1, threadId);
             ResultSet resultSet = ps.executeQuery();
@@ -53,21 +66,12 @@ public class JDBCCommentDAO implements CommentDAO {
         Thread thread = new Thread(
                 user,
                 resultSet.getString("title"),
-                resultSet.getString("text")
+                resultSet.getString("t.text")
         );
 
         Long id = resultSet.getLong("id");
 
         long parentId = resultSet.getLong("parent_comment_id");
-
-        Persisted<Long, Comment> parent = null;
-        if (parentId == 0) parent = null;
-        else {
-            Optional<Persisted<Long, Comment>> byId = findById(parentId);
-            if (byId.isPresent()) {
-                parent = byId.get();
-            }
-        }
 
         List<Persisted<Long, Comment>> persistedReplyComments = findByParentId(id);
         List<Comment> replyComments = new ArrayList<>();
@@ -84,7 +88,7 @@ public class JDBCCommentDAO implements CommentDAO {
                         resultSet.getLong("thread_id"),
                         thread
                 ),
-                parent,
+                null,
                 resultSet.getString("c.text"),
                 replyComments
         );
@@ -93,48 +97,5 @@ public class JDBCCommentDAO implements CommentDAO {
                 id,
                 comment
         );
-    }
-
-    public Optional<Persisted<Long, Comment>> findById(long id) {
-        String sql = "SELECT c.id, parent_comment_id, email, nickname, title, c.text, author_id, thread_id " +
-                    "FROM comments AS c " +
-                    "JOIN users AS u ON c.author_id = u.id " +
-                    "JOIN threads AS t ON c.thread_id = t.id " +
-                    "WHERE c.id = ?;";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setLong(1, id);
-            return findOne(ps);
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
-    }
-
-    public List<Persisted<Long, Comment>> findByParentId(long id) {
-        String sql = "SELECT c.id, parent_comment_id, email, nickname, title, c.text, author_id, thread_id " +
-                "FROM comments AS c " +
-                "JOIN users AS u ON c.author_id = u.id " +
-                "JOIN threads AS t ON c.thread_id = t.id " +
-                "WHERE parent_comment_id = ?;";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setLong(1, id);
-            ResultSet resultSet = ps.executeQuery();
-            List<Persisted<Long, Comment>> results = new ArrayList<>();
-            while (resultSet.next()) {
-                results.add(mapRowToComment(resultSet));
-            }
-            return results;
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
-    }
-
-    private Optional<Persisted<Long, Comment>> findOne(PreparedStatement ps) throws SQLException {
-        ResultSet resultSet = ps.executeQuery();
-        if (resultSet.first()) {
-            return Optional.of(mapRowToComment(resultSet));
-        } else {
-            return Optional.empty();
-        }
-
     }
 }
