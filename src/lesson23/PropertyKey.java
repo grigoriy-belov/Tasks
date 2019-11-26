5594422d07e1537f0dd03f21525efabf66a1b70a
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-11-26 13:45:41 +0200

    Clean up

--- src/lesson23/PropertyKey.java
+++ src/lesson23/PropertyKey.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(value= ElementType.FIELD)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface PropertyKey {
-    String value() default "";
+    String value();
 }
