7f3e523867d56f481684714bc8ee7434ca379fa0
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-12-24 21:57:13 +0200

    FileInputApp realization

---
+++ src/lesson26/WritingInputOnUpdateTask.java
@@ -0,0 +1,33 @@
+package lesson26;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class WritingInputOnUpdateTask implements Runnable {
+
+    private Text input;
+
+    public WritingInputOnUpdateTask(Text input) {
+        this.input = input;
+    }
+
+    @Override
+    public void run() {
+        while (!input.getText().equals("quit")) {
+            try {
+                if (input.isUpdated()) {
+                    Files.writeString(Paths.get("misc/files/output.txt"), input.getText(), StandardOpenOption.WRITE,
+                            StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+                }
+                Thread.sleep(1000);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                throw new RuntimeException();
+            }
+        }
+    }
+}
