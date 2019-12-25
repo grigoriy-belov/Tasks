4a3a8f4244d45c25fd1dc5ade5acdbe56957f707
Grigoriy Belov <grigoriy.belov1@gmail.com>
2019-12-25 09:48:15 +0200

    cleanup

--- src/lesson26/FileInputApp.java
+++ src/lesson26/FileInputApp.java
@@ -6,7 +6,6 @@ public class FileInputApp {
     public static void main(String[] args) {
 
         Text input = new Text("");
-        WritingInputOnUpdateTask writingInputOnUpdateTask = new WritingInputOnUpdateTask(input);
         new Thread(new WritingInputOnUpdateTask(input)).start();
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
--- src/lesson26/WritingInputOnUpdateTask.java
+++ src/lesson26/WritingInputOnUpdateTask.java
@@ -1,6 +1,5 @@
 package lesson26;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
