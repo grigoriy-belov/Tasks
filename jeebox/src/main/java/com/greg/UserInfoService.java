97aea63b0afcfddc626a64580ecdf8712e0b0297
Grigoriy Belov <grigoriy.belov1@gmail.com>
2020-01-17 11:16:03 +0200

    Add UserInfoService realization

--- jeebox/src/main/java/com/greg/UserInfoService.java
+++ jeebox/src/main/java/com/greg/UserInfoService.java
@@ -1,6 +1,5 @@
 package com.greg;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
---
+++ test/src/TestDogs.java
@@ -0,0 +1,9 @@
+public class TestDogs
+{
+    public static void main(String [] args)
+    {
+        Dog [][] theDogs = new Dog[3][];
+        System.out.println(theDogs[2][0].toString());
+    }
+}
+class Dog { }
