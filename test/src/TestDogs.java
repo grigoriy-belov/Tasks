5cde3676472f75750bb26ea777c2b4d75ec1ff75
Grigoriy Belov <grigoriy.belov1@gmail.com>
2020-02-03 11:51:06 +0200

    Remove redundant files

--- test/src/TestDogs.java
+++
@@ -1,9 +0,0 @@
-public class TestDogs
-{
-    public static void main(String [] args)
-    {
-        Dog [][] theDogs = new Dog[3][];
-        System.out.println(theDogs[2][0].toString());
-    }
-}
-class Dog { }
